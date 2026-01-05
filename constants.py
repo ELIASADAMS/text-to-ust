5e10fdf25e7e9fd784999c7da1bc5e06ba89412e
Eli Lab <photocakelink@gmail.com>
2026-01-05 23:08:11 +0300

    separate the functionality

---
+++ constants.py
@@ -0,0 +1,11 @@
+"""Global constants"""
+
+# Japanese
+VOWEL_CHARS = 'あいうえお'
+CONSONANT_CHARS = 'かきくけこがぎぐげござじずぜぞたちつてとだぢづでどなにぬねのはひふへほまみむめもやゆよらりるれろわをん'
+
+# ARPAsing English
+ARPABET_VOWELS = 'AA AE AH AO AW AY EH ER EY IH IY OW OY UH UW'
+ARPABET_CONSONANTS = 'B D G P T K CH F JH S SH TH V Z ZH M N NG L W Y R HH'
+
+LANGUAGE_MODES = ['japanese', 'arpasing', 'english']
