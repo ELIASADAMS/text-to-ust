18f5dfc6e2d47eaa23521995f4c3ef0aaf3ace5b
Eli Lab <photocakelink@gmail.com>
2026-01-06 05:22:11 +0300

    fix typos

--- key_roots.py
+++ key_roots.py
@@ -1,10 +1,10 @@
 """Voice key root MIDI notes"""
 
 KEY_ROOTS = {
-    "Soprano": 67,      # G4
-    "Alto": 60,         # C4
-    "Tenor": 55,        # G3
-    "Baritone": 52,     # E3
-    "Bass": 48,         # C3
+    "Soprano": 67,  # G4
+    "Alto": 60,  # C4
+    "Tenor": 55,  # G3
+    "Baritone": 52,  # E3
+    "Bass": 48,  # C3
     "C4 Default": 60
 }
