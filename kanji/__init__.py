d7f5f10745f9c84ee2acf09d59b7e04edc47ee67
Eli Lab <photocakelink@gmail.com>
2026-01-15 20:51:11 +0300

    Update __init__.py

--- kanji/__init__.py
+++ kanji/__init__.py
@@ -1,9 +1,9 @@
 """
-Kanji to Hiragana converter with Kanken level organization.
+Kanji to Hiragana converter with Kanken levels.
 """
 
-from .converter import kanji_to_hiragana, add_custom_reading, get_missing_kanji
 from .converter import KANJI_READINGS, KANKEN_LEVELS
+from .converter import kanji_to_hiragana, add_custom_reading, get_missing_kanji
 
 __all__ = ["kanji_to_hiragana", "add_custom_reading", "get_missing_kanji",
            "KANJI_READINGS", "KANKEN_LEVELS"]
