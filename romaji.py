933a323c61444ebd9e29c466d88a985952974f70
Eli Lab <photocakelink@gmail.com>
2026-01-22 02:30:43 +0300

    Update romaji.py

--- romaji.py
+++ romaji.py
@@ -1,4 +1,4 @@
 def _hiragana_to_romaji(self, mora):
-    """FIX: Reverse lookup using your HIRAGANA_MAP"""
+    """Reverse lookup using HIRAGANA_MAP"""
     reverse_map = {v: k for k, v in self.hiragana_map.items()}
     return reverse_map.get(mora, mora)
